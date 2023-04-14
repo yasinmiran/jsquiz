14e2034707ffd764c45b55dda037807f769dfdd0
Yasin <wytm97@protonmail.com>
2023-04-14 11:40:20 +0100

    change introduction section id

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const INTROUCTION_SECTION = "introduction";
+const INTROUCTION_SECTION = "my-introduction";
 const USER_INFO_SECTION = "user-info";
 const DIFFICULTY_LEVEL_SECTION =  "difficulty-level";
 const QUIZ_SECTION =  "quiz";
